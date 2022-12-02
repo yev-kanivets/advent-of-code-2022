efbe72f34adab860dd18af80c5bd427e506a7eb8
Yev Kanivets <yevhenii.kanivets@gmail.com>
2022-12-02 07:13:26 +0100

    Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,19 @@
+import java.lang.IllegalArgumentException
+
+fun main() {
+
+    fun List<String>.parse() = map { it.split(" ") }
+            .map { (it.first().first() - 'A') to (it.last().first() - 'X') }
+
+    fun part1(input: List<String>) = input
+            .parse()
+            .sumOf { (abc, xyz) -> xyz + 1 + 3 * ((4 + xyz - abc) % 3) }
+
+    fun part22(input: List<String>) = input
+            .parse()
+            .sumOf { (abc, xyz) -> xyz * 3 + 1 + (2 + xyz + abc) % 3 }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part22(input))
+}
